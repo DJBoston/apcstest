ceff4bed3a3116ce492f2efe28aecd0009968717
APCS3rd <APCS3rd@MHS-LT9-11>
2019-08-20 10:12:53 -0700

    commit 1

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/*
+ * Curtis Draheim 4th Period
+ * 
+ * A simple program that prints out "hello world" to the screen using a println function
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}

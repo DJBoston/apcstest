13c35560feaf556339fbd30561df119050f01a0b
APCS3rd <APCS3rd@MHS-LT9-11>
2019-09-24 11:00:48 -0700

    fixed bugs

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -12,17 +12,17 @@ public class QuadraticClient {
          String entry;
          
          //get a value, convert to double
-         System.out.print("a: ");
+         System.out.print("a value: ");
          entry = scaninput.nextLine();
          double a = Double.parseDouble(entry);
          
          //get b value, convert to double
-         System.out.print("b: ");
+         System.out.print("b value: ");
          entry = scaninput.nextLine();
          double b = Double.parseDouble(entry);
          
          //get c value, convert to double
-         System.out.print("c: ");
+         System.out.print("c value: ");
          entry = scaninput.nextLine();
          double c = Double.parseDouble(entry);
          
@@ -30,9 +30,9 @@ public class QuadraticClient {
          Quadratic.quadraticDescriber(a, b, c);
          
          //check to run again
-         System.out.println("\ntype quit to end or anything else to repinue");
+         System.out.println("\ntype quit to end or anything else to continue");
          entry = scaninput.nextLine();
-         if (entry.charAt(0) == 'q' || entry.charAt(0) == 'Q') {
+         if (entry.charAt(0) == 'q' || entry.charAt(0) == 'Q') { //checks to see if the first letter is q
         	 rep = false;
          
 
